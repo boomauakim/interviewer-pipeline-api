933812bcfdb818a10fa8c19f0718f7f406ee9878
boomauakim <boomauakim@gmail.com>
2024-01-18 01:50:32 +0700

    Add rate limiting

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
-import { TaskModule } from './modules/task/task.module';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { ThrottlerStorageRedisService } from 'nestjs-throttler-storage-redis';
 import { AuthModule } from './modules/auth/auth.module';
+import { TaskModule } from './modules/task/task.module';
 import { UserModule } from './modules/user/user.module';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [TaskModule, AuthModule, UserModule],
+  imports: [
+    AuthModule,
+    TaskModule,
+    ThrottlerModule.forRoot({
+      throttlers: [
+        {
+          ttl: 10000,
+          limit: 10,
+        },
+      ],
+      storage: new ThrottlerStorageRedisService(),
+    }),
+    UserModule,
+  ],
+  providers: [
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
