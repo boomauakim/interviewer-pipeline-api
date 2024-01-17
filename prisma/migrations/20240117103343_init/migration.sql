8d13f9ab739e85fd6eade611313aa1504bdd51f8
boomauakim <boomauakim@gmail.com>
2024-01-17 17:37:28 +0700

    Update database schema

--- prisma/migrations/20240117103343_init/migration.sql
+++ prisma/migrations/20240117103343_init/migration.sql
@@ -4,9 +4,10 @@ CREATE TYPE "Status" AS ENUM ('TO_DO', 'IN_PROGRESS', 'DONE');
 -- CreateTable
 CREATE TABLE "task" (
     "id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
     "title" TEXT NOT NULL,
     "description" TEXT NOT NULL,
-    "status" "Status" NOT NULL,
+    "status" "Status" DEFAULT 'TO_DO',
     "is_archived" BOOLEAN NOT NULL DEFAULT false,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -31,6 +32,7 @@ CREATE TABLE "task_changelog" (
 CREATE TABLE "task_comment" (
     "id" TEXT NOT NULL,
     "task_id" TEXT NOT NULL,
+    "user_id" TEXT NOT NULL,
     "comment" TEXT NOT NULL,
     "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" TIMESTAMP(3) NOT NULL,
@@ -53,7 +55,13 @@ CREATE TABLE "user" (
 CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
 
 -- AddForeignKey
+ALTER TABLE "task" ADD CONSTRAINT "task_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
 ALTER TABLE "task_changelog" ADD CONSTRAINT "task_changelog_task_id_fkey" FOREIGN KEY ("task_id") REFERENCES "task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE "task_comment" ADD CONSTRAINT "task_comment_task_id_fkey" FOREIGN KEY ("task_id") REFERENCES "task"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "task_comment" ADD CONSTRAINT "task_comment_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
